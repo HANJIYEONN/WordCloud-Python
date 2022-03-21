31520c3309e2ba2a5502ef28e9107276279f7abb
HANJIYEON <onedelay1@naver.com>
2022-03-21 23:43:06 +0900

    Python 워드 클라우드 API 개발하기

--- word_cloud.py
+++ word_cloud.py
@@ -9,25 +9,49 @@ import matplotlib.pyplot as plt
 # Flask 웹 서버 구축에 필요한 라이브러리를 불러옵니다.
 from flask import Flask, request, jsonify
 
-#플라스트 웹 서버 객체를 생성합니다.
+# 플라스트 웹 서버 객체를 생성합니다.
 app = Flask(__name__)
+
+def get_tags(text):
+    t = Twitter()
+    nouns = t.nouns(text)
+    count = Counter(nouns)
+    return count
+
+def process_from_text(text):
+    tags = get_tags(text)
+
+
+@app.route("/process", methods=['GET', 'POST'])
+def process():
+    content = request.json
+    words = {}
+    if content['words'] is not None:
+        for data in content['words'].values():
+            words[data['word']] = data['weight']
+    process_from_text(content['text'])
+    return jsonify(words)
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000)
 
 # 폰트 경로 설정
 font_path = 'NanumGothic.ttf'
 
-def get_tag(text, max_count, min_length):
-    # 명사만 추출합니다..
-    t = Twitter()
-    nouns = t.nouns(text)
-    processed = [n for n in nouns if len(n) >= min_length]
-    # 모든 명사의 출현 빈도를 계산합니다.
-    count = Counter(processed)
-    result = {}
-    # 출현 빈도가 높은 max_count 개의 명사만을 추출합니다.
-    for n, c in count.most_common(max_count):
-        result[n] = c
-    # 추출된 단어가 하나도 없는 경우 '내용이 없습니다.'를 화면에서 보여줍니다.
-    if len(result) == 0:
-        result["내용이 없습니다."] = 1
-    return result
-    ###..
+# def get_tag(text, max_count, min_length):
+#     # 명사만 추출합니다..
+#     t = Twitter()
+#     nouns = t.nouns(text)
+#     processed = [n for n in nouns if len(n) >= min_length]
+#     # 모든 명사의 출현 빈도를 계산합니다.
+#     count = Counter(processed)
+#     result = {}
+#     # 출현 빈도가 높은 max_count 개의 명사만을 추출합니다.
+#     for n, c in count.most_common(max_count):
+#         result[n] = c
+#     # 추출된 단어가 하나도 없는 경우 '내용이 없습니다.'를 화면에서 보여줍니다.
+#     if len(result) == 0:
+#         result["내용이 없습니다."] = 1
+#     return result
+#     ###
